c7e66db9365c956657917d205ed16a9bd868c417
YasinAtLinux <yasin1247yasin@gmail.com>
2024-09-20 17:17:57 +0530

    Create deb_login.sh
    This will log you into debian environment.

---
+++ .login scripts/deb_login.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start Debian environment 
+# DO NOT FORGET to replace your own username with yasin
+proot-distro login debian --user yasin
+
+# After logout, return control to .bashrc
+exit 0
