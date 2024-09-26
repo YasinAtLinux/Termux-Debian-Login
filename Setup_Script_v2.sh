34eceb1021d4ff271eea997d261721d78b950ceb
Yasin <yasin@localhost.localdomain>
2024-09-26 12:26:50 +0530

    Update Second Script

--- Setup_Script_v2.sh
+++ Setup_Script_v2.sh
@@ -99,4 +99,18 @@ EOF
 # Make scripts executable
 chmod +x ~/.loginScripts/*.sh
 
+# Appending Necessary lines in ~/.bashrc file
+cat << 'EOF' >> $HOME/.bashrc
+# Login and Logout from Shell :
+
+# 1. Source the initial prompt script
+bash $HOME/.loginScripts/initial_prompt.sh
+
+# 2. Check if the exit signal file exists and exit Termux if it does
+if [ -f ~/.exit_termux ]; then
+    rm ~/.exit_termux  # Remove the signal file
+    exit  # Exit Termux
+fi
+EOF
+
 echo "All scripts created and made executable!"
